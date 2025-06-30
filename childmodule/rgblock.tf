b65cdb809c202347d235a849ad684561ce441d53
sidjain22222 <sidjain22222@gmail.com>
2025-06-30 11:09:11 +0530

    newrgadd

--- childmodule/rgblock.tf
+++ childmodule/rgblock.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rgdetail" {
 name = var.rggg 
 location = var.location  
 }
+
+resource "azurerm_resource_group" "rgdetail" {
+name = var.rggg 
+location = var.location  
+}
+
